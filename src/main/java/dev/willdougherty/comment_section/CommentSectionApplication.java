c13b9ac20f1970063cf79db37b01a7538335771f
Will Dougherty <doughertyw25@gmail.com>
2025-10-23 23:48:46 +0000

    initial commit

---
+++ src/main/java/dev/willdougherty/comment_section/CommentSectionApplication.java
@@ -0,0 +1,13 @@
+package dev.willdougherty.comment_section;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CommentSectionApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CommentSectionApplication.class, args);
+	}
+
+}
---
+++ src/test/java/dev/willdougherty/comment_section/CommentSectionApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.willdougherty.comment_section;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CommentSectionApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
